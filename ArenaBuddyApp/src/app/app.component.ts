87feba44785fe4ee166cf7967536f0db2a57aa9d
burke-hill <charlesmccampbellhill@gmail.com>
2022-11-16 20:16:15 -0500

    added graph but it doesnt work

--- ArenaBuddyApp/src/app/app.component.ts
+++ ArenaBuddyApp/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Player } from './player';
 import { PlayerService } from './player.service';
-
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,21 +12,55 @@ import { PlayerService } from './player.service';
 })
 export class AppComponent implements OnInit {
 
+  chart = [];
+
   public players: Player[] = [];
+
   constructor(private playerService: PlayerService) { }
 
   ngOnInit(): void {
       this.getPlayers();
+      this.RenderChart();
   }
 
   public getPlayers(): void {
     this.playerService.getPlayers().subscribe(
       (response: Player[]) => {
         this.players = response;
+        console.log(this.players);
       },
       (error: HttpErrorResponse) => {
         console.log(error.message);
       }
     )
   }
+
+  RenderChart() {
+    const myChart = new Chart("barchart", {
+      type: 'bar',
+      data: {
+        labels: ['Druid', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: this.players,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+  }
+
+
 }
+
+
+
+
+
